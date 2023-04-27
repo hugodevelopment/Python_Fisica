59dce369758a66e553229accad6a38abdf4cef41
LucasAlves2803 <lucascosta2822@gmail.com>
2023-04-27 08:04:19 -0300

    ok

--- energia_cinetica.py
+++ energia_cinetica.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 
 massa = float(input("Insira a massa do objeto em Kg: "))
 
-velocidades = list(range(0,50,5))
+velocidades = list(range(0,50,2))
+print(velocidades)
 
 energias_cineticas = [0.5 * massa * v**2 for v in velocidades]
 
