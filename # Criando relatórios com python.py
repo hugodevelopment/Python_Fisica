dd6294b6d101d3cbb5638df870b33dba83ba2d25
LucasAlves2803 <lucascosta2822@gmail.com>
2023-05-08 19:32:48 -0300

    ok

---
+++ # Criando relatórios com python.py
@@ -0,0 +1,6 @@
+# Criando relatórios com python
+import pandas as pd
+import numpy as np
+
+dados = pd.read_excel("Dados.xlsx")
+dados
--- energia_cinetica.py
+++ energia_cinetica.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
 
+# Colocamos um valor para massa
 massa = float(input("Insira a massa do objeto em Kg: "))
 
+# Cria uma lista com os valores de velocidade, nesse caso começa do 0, vai até 50 e é de 2 em 2
 velocidades = list(range(0,50,2))
-print(velocidades)
 
+# Cria uma lista com os valores de energia cinetica
 energias_cineticas = [0.5 * massa * v**2 for v in velocidades]
 
+# Plota o gráfico
 plt.plot(velocidades,energias_cineticas)
 plt.xlabel("Velocidades m/s")
 plt.ylabel("Energia Cinetica Joules")
